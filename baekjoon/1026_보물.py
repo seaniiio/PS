f9f56d0870b73bf7aa14f8016a9462d244c263e3
seaniiio <cnc4750@naver.com>
2023-08-25 22:06:53 +0900

    solve

--- baekjoon/1026_보물.py
+++ baekjoon/1026_보물.py
@@ -5,9 +5,9 @@ arr1 = list(map(int, sys.stdin.readline().split()))
 arr2 = list(map(int, sys.stdin.readline().split()))
 
 arr1.sort()
-arr2.sort()
+arr2.sort(reverse=True)
 sum = 0
 
 for i in range(n):
-    sum += arr1[i] * arr2[n-i-1]
+    sum += arr1[i] * arr2[i]
 print(sum)
