49fc7f2d993e44a6429c3d4d8ee8f2d325c89c82
seaniiio <cnc4750@naver.com>
2024-02-04 01:19:13 +0900

    solve lv2

---
+++ Programers/LV2/숫자의_표현.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 1
+    for i in range(1, n+1):
+        temp = i
+        for j in range(i+1, n+1):
+            temp += j
+            if temp == n:
+                answer += 1
+            if temp >= n:
+                break
+    return answer
