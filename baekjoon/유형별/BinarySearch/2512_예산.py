4613f0f57ebb59029ee30c1e3a7ae4d34cfb3157
seaniiio <cnc4750@naver.com>
2024-02-04 16:37:11 +0900

    modify

--- baekjoon/유형별/BinarySearch/2512_예산.py
+++ baekjoon/유형별/BinarySearch/2512_예산.py
@@ -5,8 +5,7 @@ total = int(input())
 st, en = 1, max(a)
 while st <= en:
     mid = (st + en) // 2
-    s = sum(list(map(lambda x: x if x <= mid else mid, a)))
-    if s <= total:
+    if sum(list(map(lambda x: x if x <= mid else mid, a))) <= total:
         st = mid + 1
     else:
         en = mid - 1
