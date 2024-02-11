6a6eedbece3a021357e7f7dea27854c3c21515aa
seaniiio <cnc4750@naver.com>
2024-02-11 16:05:27 +0900

    solve g5

---
+++ baekjoon/7576_토마토.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+m, n = map(int, input().split())
+G = []
+for _ in range(n):
+    G.append(list(map(int, sys.stdin.readline().split())))
+
+visit = [[False for _ in range(m)] for _ in range(n)]
+q = deque([])
+
+for i in range(n):
+    for j in range(m):
+        if G[i][j] == 1:
+            q.append([i, j, 0])
+            visit[i][j] = True
+
+Dx = [1, -1, 0, 0]
+Dy = [0, 0, 1, -1]
+answer = 0
+
+while q:
+    x, y, day = q.popleft()
+    answer = max(answer, day)
+    for dx, dy in zip(Dx, Dy):
+        X, Y = x + dx, y + dy
+        if 0 <= X < n and 0 <= Y < m:
+            if G[X][Y] == 0 and visit[X][Y] == False:
+                visit[X][Y] = True
+                G[X][Y] = 1
+                q.append([X, Y, day + 1])
+
+for i in range(n):
+    if 0 in G[i]:
+        answer = -1
+
+print(answer)
+            
