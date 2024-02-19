3516779d8fa48493b0b5fe75ea785607d558874b
시원 <121426422+seaniiio@users.noreply.github.com>
2024-02-19 17:22:12 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31196 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -0,0 +1,41 @@
+import sys
+
+n, m = map(int, input().split())
+x, y, dir = map(int, input().split())
+G = []
+visited = [[False for _ in range(m)] for _ in range(n)]
+answer = 0
+for _ in range(n):
+    G.append(list(map(int, sys.stdin.readline().split())))
+
+# 반시계 회전
+def cir(k):
+    return 3 if k == 0 else k-1
+
+d = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+def DFS(x, y, dir):
+    global answer
+    if G[x][y] == 0:
+        G[x][y] = 2
+        answer += 1
+    exist = False
+    for nx, ny in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
+        if 0 <= nx < n and 0 <= ny < m and G[nx][ny] == 0:
+            exist = True
+    if exist:
+        while True:
+            dir = cir(dir)
+            dx, dy = x + d[dir][0], y + d[dir][1]
+            if 0 <= dx < n and 0 <= dy < m and G[dx][dy] == 0:
+                DFS(dx, dy, dir)
+                break
+    elif not exist:
+        dx, dy = x - d[dir][0], y - d[dir][1]
+        if 0 <= dx < n and 0 <= dy < m and G[dx][dy] != 1:
+            DFS(dx, dy, dir)
+        else:
+            return
+
+DFS(x, y, dir)
+print(answer)
