083f234caf7353f24ba9af12ce986f53d163d2ab
seaniiio <cnc4750@naver.com>
2024-08-12 17:54:18 +0900

    solve greedy s1

--- baekjoon/유형별/Greedy/1080_행렬.py
+++ baekjoon/유형별/Greedy/1080_행렬.py
@@ -1,35 +1,33 @@
-# 문제 이해가 어려웠음
-# 뒤집는 연산은 무조건 3x3으로만 일어날 수 있음
-# 행렬의 첫 원소부터 돌면서, 뒤집어야 하면 뒤집기연산 하고, 총 연산횟수 print
-
 import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
-a = []
-b = []
-for _ in range(n):
-    a.append(list(map(int, input().strip())))
-for _ in range(n):
-    b.append(list(map(int, input().strip())))
 
-def change(x, y):
-    global a
-    for i in range(x, x+3):
-        for j in range(y, y+3):
-            if a[i][j] == 1:
-                a[i][j] = 0
-            else:
-                a[i][j] = 1
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+m_a, m_b = [], []
+
+for _ in range(a):
+    m_a.append(list(input().strip()))
+for _ in range(a):
+    m_b.append(list(input().strip()))
 
 cnt = 0
-if n < 3 or m < 3:
-    print(-1)
-else:
-    for i in range(n-2):
-        for j in range(m-2):
-            if a[i][j] != b[i][j]:
-                change(i, j)
-                cnt += 1
-    if a != b:
-        cnt = -1
-    print(cnt)
+
+def reverse_m(i, j, m_a):
+    if i < len(m_a) - 2 and j < len(m_a[0]) - 2:
+        for _i in range(i, i+3):
+            for _j in range(j, j+3):
+                m_a[_i][_j] = '0' if m_a[_i][_j] == '1' else '1' 
+    return m_a
+
+for i in range(a):
+    for j in range(b):
+        if m_a[i][j] != m_b[i][j]:
+            m_a = reverse_m(i, j, m_a)
+            cnt += 1
+
+for i in range(a):
+    for j in range(b):
+        if m_a[i][j] != m_b[i][j]:
+            cnt = -1
+
+print(cnt)
