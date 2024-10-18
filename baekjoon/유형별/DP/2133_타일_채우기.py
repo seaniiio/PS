c07139457741537bc59fd901fac736eedfa297db
seaniiio <cnc4750@naver.com>
2024-10-18 23:50:50 +0900

    solve dp g4

---
+++ baekjoon/유형별/DP/2133_타일_채우기.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [0] * (n+1)
+
+dp[2] = 3
+
+for i in range(4, n+1):
+    if i % 2 == 1:
+        continue
+    dp[i] = dp[i-2] * 3
+    for j in range(i-4, -1, -2):
+        if j % 2 == 0:
+            dp[i] += dp[j] * 2 # 2가 특수한 경우
+    dp[i] += 2
+print(dp[n])
